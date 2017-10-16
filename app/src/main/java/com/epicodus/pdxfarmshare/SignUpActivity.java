c24569a399199859f849e2bdd37203f0dc8ccc85
nr <nicholas.raethke@gmail.com>
2017-10-15 19:22:45 -0700

    set up app with sign up and log in activities

---
+++ app/src/main/java/com/epicodus/pdxfarmshare/SignUpActivity.java
@@ -0,0 +1,164 @@
+package com.epicodus.pdxfarmshare;
+
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.Bind;
+
+public class SignUpActivity extends AppCompatActivity {
+    private static final String TAG = "SignUpActivity";
+
+    @Bind(R.id.input_name) EditText _nameText;
+    @Bind(R.id.input_address) EditText _addressText;
+    @Bind(R.id.input_email) EditText _emailText;
+    @Bind(R.id.input_mobile) EditText _mobileText;
+    @Bind(R.id.input_password) EditText _passwordText;
+    @Bind(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
+    @Bind(R.id.btn_signUp) Button _signUpButton;
+    @Bind(R.id.link_login) TextView _loginLink;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface rustNS = Typeface.createFromAsset(getAssets(), "fonts/rust_never_sleeps.ttf");
+        mAppNameTextView.setTypeface(rustNS);
+
+        _signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void signUp() {
+        Log.d(TAG, "SignUp");
+
+        if (!validate()) {
+            onSignUpFailed();
+            return;
+        }
+
+        _signUpButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        // TODO: Implement your own signUp logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        onSignUpSuccess();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignUpSuccess() {
+        _signUpButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignUpFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _signUpButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            _nameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (address.isEmpty()) {
+            _addressText.setError("Enter Valid Address");
+            valid = false;
+        } else {
+            _addressText.setError(null);
+        }
+
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (mobile.isEmpty() || mobile.length()!=10) {
+            _mobileText.setError("Enter Valid Mobile Number");
+            valid = false;
+        } else {
+            _mobileText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+            _reEnterPasswordText.setError("Password Do not match");
+            valid = false;
+        } else {
+            _reEnterPasswordText.setError(null);
+        }
+
+        return valid;
+    }
+}
